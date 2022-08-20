58b7838073fd543367f8035dfaff404d899e74dc
vsfd <vsfd@gft.com>
2022-08-20 16:33:49 -0300

    Criação da estrutura do projeto

---
+++ Application/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
